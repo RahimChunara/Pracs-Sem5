24db3be559aa2ffee8bbabad8f4ee3c6c2377331
Rahim Chunara <rahimchunara55@mgial.com>
2019-10-27 20:23:01 +0530

    Updated

--- WT/CRUD/index.php
+++ WT/CRUD/index.php
@@ -1,3 +1,11 @@
+<!-- MYSQL DB  -->
+<!-- CREATE TABLE employees (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    salary INT(10) NOT NULL
+); -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
